a050ea33a1bdb44fc22bda2b8ed572e456c1d278
Minhyeok Park <pmh_only@pmh.codes>
2024-02-26 15:29:30 +0900

    test: log client thing

--- worker/bot/responder.go
+++ worker/bot/responder.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"log"
+
 	"github.com/disgoorg/disgo/discord"
 	"github.com/pmh-only/proj.fn/worker/env"
 )
@@ -9,6 +11,8 @@ func (b Bot) respond(content discord.MessageUpdate) {
 	if env.DISCORD_INTERACTION_TOKEN == nil {
 		return
 	}
+
+	log.Println(b.client)
 
 	b.client.Rest().UpdateInteractionResponse(
 		env.DISCORD_APPLICATION_ID,
